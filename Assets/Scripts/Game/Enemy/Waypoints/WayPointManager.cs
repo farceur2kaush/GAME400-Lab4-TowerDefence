1857b29c2312209fcf44f6773476489b9f310228
Yash Kaushik <farceur.kaush@gmail.com>
2018-11-22 10:17:15 -0500

    - Waypoints system setup

--- Assets/Scripts/Game/Enemy/Waypoints/WayPointManager.cs
+++ Assets/Scripts/Game/Enemy/Waypoints/WayPointManager.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class WayPointManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Refrence to this script
+    public static WayPointManager Instance;
+    
+    // List that stores the waypoints
+    public List<Path> Paths = new List<Path>();
+    void Awake()
+    {
+        // Singleton Pattern - makes sure only one instance of this is generated
+        Instance = this;
+    }
+    // Return position of waypoint where the enemy is spawned
+    public Vector3 GetSpawnPosition(int pathIndex)
+    {
+        return Paths[pathIndex].WayPoints[0].position;
+    }
 }
+
+[System.Serializable]
+public class Path
+{
+    public List<Transform> WayPoints = new List<Transform>();
+}
+
